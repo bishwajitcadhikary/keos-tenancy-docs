f5384b5c6b2fe2267539dd6d61d24e4607fd1da4
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2024-03-10 13:03:52 +0600

    feat: add others

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>WovoSaas</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/bishwajitcadhikary/wovosaas/tree/main',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/bishwajitcadhikary/keos-tenancy-docs/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: new Date().getFullYear() + ' © Keos LLC',
   },
 }
 
